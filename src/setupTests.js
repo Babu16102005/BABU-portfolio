9b461eedb89d2b685629d6ae78ddd02091873902
https://github.com/Dhivagar13/CareerChatBot/tree/Frontend <babusanthosh6381@gmail.com>
2025-10-17 22:58:10 +0530

    portfolio update

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,10 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: 'https://babu16102005.github.io/', // <-- Add this line
-});

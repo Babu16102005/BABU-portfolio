0eccfd1507642a56d24739f614da35975f61365f
https://github.com/Dhivagar13/CareerChatBot/tree/Frontend <babusanthosh6381@gmail.com>
2025-10-18 23:04:32 +0530

    portfolio update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/BABU-portfolio/', // <-- important for GitHub Pages
+  base: '/', // <-- important for GitHub Pages
 });
